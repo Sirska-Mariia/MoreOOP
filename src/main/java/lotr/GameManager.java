fa212e0a9379d91d1c8af57e94ee76c2134377ca
Sirska-Mariia <149333906+Sirska-Mariia@users.noreply.github.com>
2024-10-04 19:06:59 +0300

    added game manager

--- src/main/java/lotr/GameManager.java
+++ src/main/java/lotr/GameManager.java
@@ -1,5 +1,26 @@
 package lotr;
 
 public class GameManager {
-
-}
+    public void fight(Character c1, Character c2) {
+        System.out.println("Fight starts between two characters.");
+    
+        while(c1.isAlive() && c2.isAlive()) {
+            System.out.println("Character 1 attacks Character 2");
+            c1.kick(c2);
+    
+            if(!c2.isAlive()) {
+                System.out.println("Character 2 has been defeated!");
+                break;
+            }
+            System.out.println("Character 2 attacks Character 1");
+            c2.kick(c1);
+    
+            if(!c1.isAlive()) {
+                System.out.println("Character 1 has been defeated!");
+                break;
+            }
+        }
+    
+        System.out.println("The fight is over.");
+    }
+} 
